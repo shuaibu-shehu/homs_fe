f2f75de076e8d760f9601c5463725e2488e483af
ajay-v11 <heraappollo@gmail.com>
2024-11-18 18:57:05 +0530

    homepage

--- src/components/home/features.tsx
+++ src/components/home/features.tsx
@@ -1,31 +1,43 @@
-import React from 'react'
-import MaxWidthWrapper from '../max-width-wrapper'
-import { LucideAlertCircle, LucideBarChart2, LucideMonitor } from 'lucide-react'
+import React from "react"
+import MaxWidthWrapper from "../max-width-wrapper"
+import {LucideAlertCircle, LucideBarChart2, LucideMonitor} from "lucide-react"
 
 function Features() {
   return (
     <MaxWidthWrapper className=' mt-6'>
-        <h1  className=' my-6 text-center font-bold text-3xl'>Features</h1>
-       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-4'>
-          <div className=' h-[350px] bg-green-800/100  shadow-2xl text-white flex flex-col items-center text-left gap-4 max-w-[300px]  leading-7 border-2 p-6 rounded-lg'>
-            <LucideMonitor color='white'   size={40} />
-            <div>
-            Monitor individual patient oxygen consumption in real time, ensuring each patient receives the exact amount of oxygen they need based on their condition. The data is tracked live to allow immediate response by healthcare providers.
-            </div>
+      <h1 className=' my-6 text-center font-bold text-4xl p-10 text-[#4d4d4d]'>
+        Features
+      </h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-4'>
+        <div className=' h-[350px] bg-[#f5f7fa]  shadow-2xl text-[#4d4d4d] flex flex-col items-center text-left gap-4 max-w-[300px]  leading-7 border-2 p-6 rounded-lg'>
+          <LucideMonitor color='#4cac4c' size={40} />
+          <div>
+            Monitor individual patient oxygen consumption in real time, ensuring
+            each patient receives the exact amount of oxygen they need based on
+            their condition. The data is tracked live to allow immediate
+            response by healthcare providers.
           </div>
-          <div className='h-[350px] bg-green-800/100  shadow-2xl text-white flex flex-col items-center text-left gap-4 max-w-[300px] leading-7 border-2 p-6 rounded-lg'>
-            <LucideBarChart2 color='white'   size={40} />
-            <div>
-            Keep track of the overall oxygen consumption from the central planted tank. The system continuously monitors the tank&apos;s levels and usage, allowing for efficient management and timely refills, ensuring no interruptions in oxygen supply            </div>
+        </div>
+        <div className='h-[350px] bg-[#f5f7fa] shadow-2xl text-[#4d4d4d] flex flex-col items-center text-left gap-4 max-w-[300px] leading-7 border-2 p-6 rounded-lg'>
+          <LucideBarChart2 color='#4cac4c' size={40} />
+          <div>
+            Keep track of the overall oxygen consumption from the central
+            planted tank. The system continuously monitors the tank&apos;s
+            levels and usage, allowing for efficient management and timely
+            refills, ensuring no interruptions in oxygen supply{" "}
           </div>
-          <div className='h-[350px] bg-green-800/100  shadow-2xl text-white flex flex-col items-center text-left gap-4 max-w-[300px] leading-7 border-2 p-6 rounded-lg'>
-            <LucideAlertCircle color='white'   size={40} />
-            <div>
-            Get instant alerts if patient oxygen levels fluctuate or if the central tank’s oxygen supply drops below critical levels. These real-time notifications help prevent emergencies and ensure uninterrupted oxygen delivery            </div>
+        </div>
+        <div className='h-[350px] bg-[#f5f7fa]  shadow-2xl text-[#4d4d4d] flex flex-col items-center text-left gap-4 max-w-[300px] leading-7 border-2 p-6 rounded-lg'>
+          <LucideAlertCircle color='#4cac4c' size={40} />
+          <div>
+            Get instant alerts if patient oxygen levels fluctuate or if the
+            central tank’s oxygen supply drops below critical levels. These
+            real-time notifications help prevent emergencies and ensure
+            uninterrupted oxygen delivery{" "}
           </div>
-
-       </div>
-   </MaxWidthWrapper>
+        </div>
+      </div>
+    </MaxWidthWrapper>
   )
 }
 
--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import CTAButton from '../CTA-button'
-import MaxWidthWrapper from '../max-width-wrapper'
-import Image from 'next/image'
+import React from "react"
+import CTAButton from "../CTA-button"
+import MaxWidthWrapper from "../max-width-wrapper"
+import Image from "next/image"
 
 function Hero() {
   return (
-    <MaxWidthWrapper className='flex justify-center flex-col items-center gap-7'>
-      <div className=' flex flex-col md:flex-row justify-between items-center gap-6'>
+    <MaxWidthWrapper className='flex justify-center flex-col items-center gap-7 '>
+      <div className=' flex flex-row justify-between items-center gap-20 '>
         <div className='flex-1 flex flex-col gap-4'>
-          <h1 className='text-4xl font-bold text-gray-800'>
-            Optimizing Oxygen Usage for Better Patient Care with Real-Time Monitoring and AI Predictive Analytics
+          <h1 className='text-6xl font-bold text-[#4d4d4d]'>
+            Hospital <span className='text-[#4caf4f]'>Oxygen Management </span>
+            System
           </h1>
-          <p className='text-gray-600'>
-            Track and predict oxygen consumption across hospital wards, ensuring timely and efficient oxygen supply for patients.
+          <p className='text-[#808080] font-normal'>
+            Track and predict oxygen consumption across hospital wards, ensuring
+            timely and efficient oxygen supply for patients.
           </p>
         </div>
         <div className='flex-1'>
-          <Image src={"/hero.png"} className='rounded-lg flex-1 w-[400px] h-[400px]' alt='hero'  width={500} height={500} />
+          <Image
+            src={"/hero.png"}
+            className='rounded-lg flex-1 w-[400px] h-[400px]'
+            alt='hero'
+            width={500}
+            height={500}
+          />
         </div>
       </div>
 
-      <CTAButton className=' flex h-[50px] font-bold rounded-lg bg-green-900 hover:bg-green-800 w-60 my-6' >
-        <span className='p-5 my-4 text-lg'>
-        Get started
-        </span>
-        </CTAButton>
+      <CTAButton className=' flex h-[50px] font-bold rounded-lg bg-[#4caf4f] hover:bg-[#263238] w-60 my-6'>
+        <span className='p-5 my-4 text-lg'>Get started</span>
+      </CTAButton>
     </MaxWidthWrapper>
   )
 }
